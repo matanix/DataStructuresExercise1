5968345e7178885f3d3fbc0b505020964d5a569c
Omri Siniver <omri.siniver1@gmail.com>
2019-09-23 19:26:59 +0300

    create 50 operations

--- DataStructuresExercise1/main.cpp
+++ DataStructuresExercise1/main.cpp
@@ -2,17 +2,18 @@
 #include "AuctionHeap.h"
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-#define MAX_AUCTION_HEAP_SIZE 10
+static constexpr unsigned int MAX_AUCTION_HEAP_SIZE = 20;
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	srand(unsigned int (time(NULL)));
+	srand(static_cast<unsigned int>(time(nullptr)));
 	AuctionHeap<MAX_AUCTION_HEAP_SIZE> auctionHeap;
 	
-	for (int i = 0; i < MAX_AUCTION_HEAP_SIZE; i++)
+	for (int i = 0; i < MAX_AUCTION_HEAP_SIZE / 2; i++)
 	{
 		unsigned int randVal = rand() % 100 + 1;
 		auctionHeap.Insert(randVal);
@@ -24,14 +25,31 @@ int main(int argc, char const *argv[])
 
 	cout << "Max is " << resultVal << " \n" ;
 
-	for (int place = 1; place <= MAX_AUCTION_HEAP_SIZE; place++)
+	for (int place = 1; place <= MAX_AUCTION_HEAP_SIZE / 2; place++)
 	{
 		auctionHeap.GetBidByPlace(place, resultVal);
 
 		cout << "Val at place " << place << " is " << resultVal << " \n";
 	}
 
+	for (int i = 0; i < MAX_AUCTION_HEAP_SIZE; i++)
+	{
+		unsigned int randVal = rand() % 100 + 1;
+		auctionHeap.Insert(randVal);
+		cout << "Inserted " << randVal << " \n";
+	}
 
+	resultVal = 0;
+	auctionHeap.GetMax(resultVal);
+
+	cout << "Max is " << resultVal << " \n" ;
+
+	for (int place = 1; place <= MAX_AUCTION_HEAP_SIZE; place++)
+	{
+		auctionHeap.GetBidByPlace(place, resultVal);
+
+		cout << "Val at place " << place << " is " << resultVal << " \n";
+	}
 
 	return 0;
 }
